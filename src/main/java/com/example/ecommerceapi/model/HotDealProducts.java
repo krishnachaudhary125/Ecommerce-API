141789009ed5b261bdeaef5ebbd167df3442faed
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-08-14 14:17:27 +0545

    feat: entity for hot deals product

---
+++ src/main/java/com/example/ecommerceapi/model/HotDealProducts.java
@@ -0,0 +1,37 @@
+package com.example.ecommerceapi.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        name = "hot_deal_products",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"product_id"})
+        }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class HotDealProducts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void onCreate(){
+        createdAt = LocalDateTime.now();
+    }
+}
