83637da2935da42ddf2b505b12981b5802c623d5
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-08-14 15:55:47 +0545

    fix: controller for popular brand

---
+++ src/main/java/com/example/ecommerceapi/controller/PopularBrandProductController.java
@@ -0,0 +1,27 @@
+package com.example.ecommerceapi.controller;
+
+import com.example.ecommerceapi.dto.ProductResponse;
+import com.example.ecommerceapi.service.PopularBrandProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/popular-brand-product")
+@RequiredArgsConstructor
+public class PopularBrandProductController {
+
+    private final PopularBrandProductService popularBrandProductService;
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getPopularBrandProduct() {
+
+        return ResponseEntity.ok(
+                popularBrandProductService.getPopularBrandProducts()
+        );
+    }
+}
