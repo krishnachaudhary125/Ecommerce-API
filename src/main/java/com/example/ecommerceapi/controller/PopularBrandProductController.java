13bb260cb6a6bd083259207db5d38076c5111c2b
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-08-14 17:44:45 +0545

    fix: minor

--- src/main/java/com/example/ecommerceapi/controller/PopularBrandProductController.java
+++ src/main/java/com/example/ecommerceapi/controller/PopularBrandProductController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/popular-brand-product")
+@RequestMapping("/popular-brand-products")
 @RequiredArgsConstructor
 public class PopularBrandProductController {
 
