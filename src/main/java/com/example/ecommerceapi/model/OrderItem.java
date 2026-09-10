3916f974f1a7bd1a4b7f8bbf1ec3a39c3ffd0e71
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-09-10 17:28:39 +0545

    feat: created order item model

---
+++ src/main/java/com/example/ecommerceapi/model/OrderItem.java
@@ -0,0 +1,40 @@
+package com.example.ecommerceapi.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "order_items")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(name = "product_title", nullable = false)
+    private String productTitle;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private Double subtotal;
+}
