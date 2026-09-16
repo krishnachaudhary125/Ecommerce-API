2728021e41e01f6f8641eb25889219cb9bc50a6a
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-09-16 14:31:49 +0545

    feat: added endpoint to fetch single order or whole order

--- src/main/java/com/example/ecommerceapi/controller/OrderController.java
+++ src/main/java/com/example/ecommerceapi/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.example.ecommerceapi.controller;
 
 import com.example.ecommerceapi.dto.CreateOrderRequest;
+import com.example.ecommerceapi.dto.OrderDetailResponse;
 import com.example.ecommerceapi.dto.OrderResponse;
 import com.example.ecommerceapi.model.Order;
 import com.example.ecommerceapi.service.OrderService;
@@ -8,6 +9,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/orders")
@@ -25,6 +28,31 @@ public class OrderController {
 
         return ResponseEntity.ok(
                 OrderResponse.from(order)
+        );
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderDetailResponse>> getUserOrders() {
+
+        List<Order> orders = orderService.getUserOrders();
+
+        List<OrderDetailResponse> response = orders
+                .stream()
+                .map(OrderDetailResponse::from)
+                .toList();
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderDetailResponse> getOrderDetail(
+            @PathVariable Long orderId
+    ) {
+
+        Order order = orderService.getOrderDetail(orderId);
+
+        return ResponseEntity.ok(
+                OrderDetailResponse.from(order)
         );
     }
 }
